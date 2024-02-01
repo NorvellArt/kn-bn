0a3a3ac90fc0282547f12db65af9d9e27971dd7a
Artem Batorshin <a.batorshin@gandiva.ru>
2024-02-01 21:11:39 +0300

    Start reworking server side

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,26 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '@/app.controller';
-import { AppService } from '@/app.service';
-import { AuthModule } from '@/auth/auth.module';
-import { UsersModule } from '@/users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import ormConfig from '@/config/orm.config';
-import ormConfigProd from '@/config/orm.config.prod';
 
 @Module({
-  imports: [
-    AuthModule,
-    UsersModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [ormConfig],
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
--- server/src/users/users.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from '@nestjs/common';
-
-@Module({
-  exports: [UsersModule],
-})
-export class UsersModule {}
