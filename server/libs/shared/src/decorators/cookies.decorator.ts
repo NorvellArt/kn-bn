468c90e9042625e14721fb6b465a40ad8ca1dbd8
Artem Batorshin <a.batorshin@gandiva.ru>
2024-02-04 21:02:44 +0300

    Add logout method

--- server/libs/shared/src/decorators/cookies.decorator.ts
+++ server/libs/shared/src/decorators/cookies.decorator.ts
@@ -3,5 +3,5 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 export const Cookie = createParamDecorator((key: string, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
 
-    return key && key in req.cookies ? req.cookies[key] : req.cookies;
+    return key && key in req.cookies ? req.cookies[key] : key ? null : req.cookies;
 });
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -96,4 +96,8 @@ export class AuthService {
             },
         });
     }
+
+    deleteRefreshToken(token: string) {
+        return this.prismaService.token.delete({ where: { token } });
+    }
 }
