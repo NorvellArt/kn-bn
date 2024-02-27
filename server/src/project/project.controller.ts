1d774234f96f578e66934ed144653adc312c792c
Artem Batorshin <a.batorshin@gandiva.ru>
2024-02-27 22:55:47 +0300

    BE: Project updating

--- server/src/project/project.controller.ts
+++ server/src/project/project.controller.ts
@@ -1,8 +1,9 @@
 import { CurrentUser } from '@/shared/decorators';
-import { Controller, Post, Body } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Controller, Post, Body, Put, Param } from '@nestjs/common';
+import { Project, User } from '@prisma/client';
 import { CreateProjectDto } from './dto';
 import { ProjectService } from './project.service';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Controller('project')
 export class ProjectController {
@@ -11,5 +12,13 @@ export class ProjectController {
     @Post()
     async create(@Body() input: CreateProjectDto, @CurrentUser() user: User) {
         return this.projectService.createProjects(input, user);
+    }
+
+    @Put(':id')
+    async publishPost(@Param('id') id: string, @Body() input: UpdateProjectDto): Promise<Project> {
+        return this.projectService.updateProject({
+            where: { id },
+            data: { ...input },
+        });
     }
 }
